6ced61319b691ddadc9752305c318fb7fac5d588
codeiffor <sid96@protonmail.com>
2019-01-02 23:53:36 +0530

    MXM.py

--- practice/MXM.py
+++ practice/MXM.py
@@ -2,10 +2,13 @@ t=int(input())
 for _ in range(t):
     x=input()
     a=[int(x)+1 for x in input().split()]
-    x,l,r=1,0,sum(a)
+    x,l=1,0
+    r=1
+    for tmp in a:
+        r*=tmp
     for i in range(len(a)-1):
-        l+=a[i]
-        r-=a[i]
+        l*=a[i]
+        r/=a[i]
         if(l<=r):
             x=i+1
         else:
